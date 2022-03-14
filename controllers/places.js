ded55736d8b4c844067e877859f4ef40f99efa8a
Keoki <keoki.alan.mundon@gmail.com>
2022-03-14 11:52:08 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/new', (req, res) => {
     res.render('places/new')
@@ -6,26 +7,21 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
   console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
 
 router.get("/", (req, res) => {
-    let places = [
-      {
-        name: "H-Thai-ML",
-        city: "Seattle",
-        state: "WA",
-        cuisines: "Thai, Pan-Asian",
-        pic: "images/vicky-ng-NT5oqzp-050-unsplash.jpg",
-      },
-      {
-        name: "Coding Cat Cafe",
-        city: "Phoenix",
-        state: "AZ",
-        cuisines: "Coffee, Bakery",
-        pic: "images/jeremy-yap-jn-HaGWe4yw-unsplash.jpg",
-      },
-    ];
     res.render("places/index", { places });
   });
   
