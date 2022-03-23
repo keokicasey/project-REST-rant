ee4fdef7b82059507154771b6bc80caab8efb0e3
Keoki <keoki.alan.mundon@gmail.com>
2022-03-22 19:46:11 -0700

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -1,41 +1,67 @@
-const router = require('express').Router()
-const places = require('../models/places.js')
+const router = require("express").Router();
+const places = require("../models/places.js");
 
 router.get("/", (req, res) => {
   res.render("places/index", { places });
 });
 
-
-
-router.post('/', (req, res) => {
-  console.log(req.body)
+router.post("/", (req, res) => {
   if (!req.body.pic) {
     // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
+    req.body.pic = "http://placekitten.com/400/400";
   }
   if (!req.body.city) {
-    req.body.city = 'Anytown'
+    req.body.city = "Anytown";
   }
   if (!req.body.state) {
-    req.body.state = 'USA'
+    req.body.state = "USA";
   }
-  places.push(req.body)
-  res.redirect('/places')
-})
+  console.log(req.body);
+  places.push(req.body);
+  res.redirect("/places");
+});
 
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
 
-router.get('/:id', (req, res) => {
-  let id = Number(req.params.id)
+router.get("/:id", (req, res) => {
+  let id = Number(req.params.id);
   if (isNaN(id)) {
-    res.render('error404')
+    res.render("error404");
   } else if (!places[id]) {
-    res.render('error404')
+    res.render("error404");
   } else {
-    res.render('places/show', { place: places[id] })
+    res.render("places/show", { place: places[id], id });
   }
-})
+});
+
+router.put("/:id", (req, res) => {
+  res.send("Update a particular place");
+});
+
+router.get("/:id/edit", (req, res) => {
+  res.send("Form page for editing an existing place");
+});
+
+router.delete("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    places.splice(id, 1);
+    res.redirect("/places");
+  }
+});
+
+router.post("/:id/rant", (req, res) => {
+  res.send("Create a rant (comment) about a particular place");
+});
+
+router.delete("/:id/rant/:rantId", (req, res) => {
+  res.send("Delete a rant (comment) about a particular place");
+});
 
 module.exports = router;
